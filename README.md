b6946c17a5d600a9e6c3c915112b18db7b78835a
Photon <wong.wk1487@gmail.com>
2017-06-01 11:16:05 +0800

    Updated files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # le-buggy
 Common packages used in the autonomous golf buggy project.
 
+## Packages
+Packages to be installed system-wide:
+- Navigation stack
+- Hector SLAM
--- buggy_control/src/steering_params.cpp
+++ buggy_control/src/steering_params.cpp
@@ -8,7 +8,7 @@ ros::Publisher jt_pub;
 ros::Subscriber sub;
 geometry_msgs::Twist cmd_vel;
 
-std::string type; 
+std::string type;
 int linearX,linearY,angularZ, brake, unlock, home;
 bool initialized=false;
 
@@ -17,32 +17,32 @@ bool initialized=false;
 void JoyCallback( const sensor_msgs::Joy::ConstPtr& js)
 {
 	bool   unlockOn   = false;
-	if(type=="gamepad") 
+	if(type=="gamepad")
 	{
 		cmd_vel.linear.x  = js->axes[linearX]; //to/fro
 		cmd_vel.linear.z  = js->buttons[brake];             //brake
 		cmd_vel.linear.y  = js->buttons[unlock];            //unlock
-		cmd_vel.angular.z = js->axes[angularZ]; //rotate left/right 
+		cmd_vel.angular.z = js->axes[angularZ]; //rotate left/right
 		cmd_vel.angular.x = js->buttons[home];    //homing
-		
+
 	}
-	else if (type=="joystick") 
+	else if (type=="joystick")
 	{
 		cmd_vel.linear.x  = js->axes[linearX]; //to/fro
 		cmd_vel.linear.z  = js->buttons[brake];             //brake
 		cmd_vel.linear.y  = js->buttons[unlock];            //unlock
-		cmd_vel.angular.z = js->axes[angularZ]; //rotate left/right 
-		cmd_vel.angular.x = js->axes[home];    //homing	
+		cmd_vel.angular.z = js->axes[angularZ]; //rotate left/right
+		cmd_vel.angular.x = js->axes[home];    //homing
 	}
-	else 
+	else
 	{
-		cmd_vel.linear.x =  0.0; 
+		cmd_vel.linear.x =  0.0;
 		cmd_vel.angular.z = 0.0;
-		cmd_vel.linear.y  = 0.0; 
-		ROS_INFO("No valid controller");	
+		cmd_vel.linear.y  = 0.0;
+		ROS_INFO("No valid controller");
 	}
-	
-		unlockOn   = cmd_vel.linear.y;	
+
+		unlockOn   = cmd_vel.linear.y;
 		if( cmd_vel.linear.z==1.0 )
 		{
 			cmd_vel.linear.x = 0.0;
@@ -52,10 +52,10 @@ void JoyCallback( const sensor_msgs::Joy::ConstPtr& js)
 		{
 			cmd_vel.angular.z = 0.0;
 			cmd_vel.linear.x  = 0.0;
-		}	
-	
+		}
+
 	ROS_INFO("%s: cmd_vel: lx:%.3f,ly:%.3f,lz:%.3f,az:%.3f,ax:%.3f", type.c_str(), cmd_vel.linear.x, cmd_vel.linear.y,cmd_vel.linear.z, cmd_vel.angular.z, cmd_vel.angular.x);
-	
+
 	jt_pub.publish( cmd_vel );
 }
 
@@ -63,32 +63,32 @@ int main(int argc, char** argv)
 {
     ros::init(argc,argv,"buggy");
     ros::NodeHandle nh("steering");
-    
+
     nh.param<std::string>("type", type,"null");
     nh.param("linearX",linearX,0);
     nh.param("linearY",linearY,3);
     nh.param("angularZ",angularZ,2);
-    nh.param("brake", brake ,0);	
+    nh.param("brake", brake ,0);
     nh.param("unlock",unlock,1);
     nh.param("home",home,0);
-    
+
     cmd_vel.linear.x=0.0;
     cmd_vel.linear.y=0.0;
     cmd_vel.linear.z=0.0;
     cmd_vel.angular.x=0.0;
     cmd_vel.angular.y=0.0;
     cmd_vel.angular.z=0.0;
-  
-    jt_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1);
+
+    jt_pub = nh.advertise<geometry_msgs::Twist>("/joy_vel",1);	//Changed from cmd_vel -> joy_vel
     sub=nh.subscribe<sensor_msgs::Joy>("/joy",10, JoyCallback);
 
     ROS_INFO("%s: Axes: %d, %d, %d",type.c_str(),linearX, linearY, angularZ);
-    
+
     while(ros::ok())
     {
     	ros::spinOnce();
     }
-	
-	
+
+
 	return 0;
 }
